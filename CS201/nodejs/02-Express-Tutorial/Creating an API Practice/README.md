88266f9014cfcd9e95d5f4ec8ecc92ea3f336586
Ilya Tate <ilyatate@gmail.com>
2021-10-12 13:07:32 -0700

    Class 10/07/21

---
+++ CS201/nodejs/02-Express-Tutorial/Creating an API Practice/README.md
@@ -0,0 +1,20 @@
+# Menu API
+
+## GET
+
+- ### API Endpoint
+    GET `localhost:3000/api/v1/`
+
+- ### Find by "id"
+    GET `/api/v1/find/id/:id`
+
+- ### Find by "category"
+    GET `/api/v1/category/:category`
+
+- ### Sort by "price"
+    GET `/api/v1/sort/price/query?`
+
+    #### Parameters
+
+    - `dir=asc` or `dir=ascending`
+    - `dir=dec` or `dir=descending`
--- CS201/nodejs/02-Express-Tutorial/Creating an API Practice/index.js
+++ CS201/nodejs/02-Express-Tutorial/Creating an API Practice/index.js
@@ -5,7 +5,48 @@ const app = express();
 const port = 3000;
 
 app
-  .get("/", (req, res) => {
-    req.json(menu);
+  .get("/api/v1", (req, res) => {
+    let newMenu = {};
+    menu.map((item) => {
+      const { title, category } = item;
+      if (!newMenu[category]) newMenu[category] = [];
+      newMenu[category].push(title);
+    });
+
+    res.json(newMenu);
+  })
+  .get("/api/v1/find/id/:id", (req, res) => {
+    const { id } = req.params;
+    const foundItem = menu.find((item) => {
+      return item.id === +id;
+    });
+
+    if (!foundItem) {
+      res.status(404).json({ result: null, error: "Not found" });
+    }
+    res.json({ result: foundItem, message: "Found" });
+  })
+  .get("/api/v1/find/category/:category", (req, res) => {
+    const { category } = req.params;
+    const foundItems = menu.filter((item) => {
+      return item.category === category;
+    });
+
+    if (!foundItems.length) {
+      res.status(404).json({ result: null, error: "Not found" });
+    }
+    res.json({ result: foundItems, message: "Found" });
+  })
+  .get("/api/v1/sort/price/query", (req, res) => {
+    const { dir } = req.query;
+
+    let sortedItems = [...menu];
+    if (["asc", "ascending"].includes(dir)) {
+      sortedItems.sort((a, b) => a.price - b.price);
+    }
+    if (["dec", "descending"].includes(dir)) {
+      sortedItems.sort((a, b) => b.price - a.price);
+    }
+    res.json(sortedItems);
   })
   .listen(port, () => console.log(`Listening on port ${port}`));
